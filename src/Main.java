6b5609f55194d3dbdc39ecbb94f1d07af89c3a32
Nikita <trenin.0235@gmail.com>
2024-03-31 16:57:03 +0700

    Топчик дима

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println(Core.VERSION);
+
+
+        System.out.println("dima");
     }
 }
