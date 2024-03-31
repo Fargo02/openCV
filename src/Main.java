aa001ba35f586d3c49c0f4771762779400fdb5ae
Nikita <trenin.0235@gmail.com>
2024-03-31 21:43:21 +0700

    Распознование лица

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,48 @@
-import org.opencv.core.Core;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println(Core.VERSION);
 
-        int a = 0;
+        CascadeClassifier faceDetector
+                = new CascadeClassifier();
+        faceDetector.load("C:/Users/Nikita/IdeaProjects/openCV/src/resources/haarcascade_frontalface_alt.xml");
 
-        while (true) {
-            a++;
-            System.out.println(a);
-            if (a == 3) {
-                break;
-            }
+        Mat image = Imgcodecs.imread("C:/Users/Nikita/IdeaProjects/openCV/src/resources/dima.jpg");
+
+        // Detecting faces
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image,
+                faceDetections);
+
+        for (Rect rect : faceDetections.toArray()) {
+            Imgproc.rectangle(
+                    image, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width,
+                            rect.y + rect.height),
+                    new Scalar(0, 255, 0));
         }
 
-        System.out.println("dima");
+        // Saving the output image
+        String filename = "Ouput.jpg";
+
+        Imgcodecs.imwrite("D:/" + filename, image);
+
+        System.out.print("Face Detected");
+
+        System.exit(0);
+    }
+
+    public static Mat loadImage(String imagePath) {
+        Imgcodecs imageCodecs = new Imgcodecs();
+        return imageCodecs.imread(imagePath);
+    }
+    public static void saveImage(Mat imageMatrix, String targetPath) {
+        Imgcodecs imgcodecs = new Imgcodecs();
+        imgcodecs.imwrite(targetPath, imageMatrix);
     }
 }
