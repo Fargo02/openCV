3b0470d26a9d98aa3f4de1f68521a80061cafb11
Nikita <trenin.0235@gmail.com>
2024-03-31 16:12:27 +0700

    аааа

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import org.opencv.core.Core;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.VERSION);
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.VERSION);
+    }
+}
