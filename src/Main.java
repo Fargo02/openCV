f62c6c3fe4d539561c5e439704533b42fc77d6b7
Nikita <trenin.0235@gmail.com>
2024-04-04 18:00:16 +0700

    Настроил проект 123134

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,8 @@ public class Main {
         Rect rectCrop = null;
         Tesseract tesseract = new Tesseract();
         CascadeClassifier faceDetector = new CascadeClassifier();
-
+        tesseract.setLanguage("rus");
+        tesseract.setTessVariable("tessedit_char_whitelist","0123456789АВЕКМНОРСТУХ");
 
         faceDetector.load("src/resources/haarcascade_russian_plate_number.xml");
         Mat image = Imgcodecs.imread("src/resources/bmw.jpg");
@@ -25,31 +26,31 @@ public class Main {
         faceDetector.detectMultiScale(image, faceDetections);
 
 
-
         for (Rect rect : faceDetections.toArray()) {
             Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 0, 255), 4);
             rectCrop = new Rect(rect.x, rect.y, rect.width, rect.height);
         }
 
-        String filename = "plate_numbe.jpg";
+        String filename = "plate_number.jpg";
 
         Mat markedImage = new Mat(image,rectCrop);
 
-        Imgcodecs.imwrite("C:\\Users\\Nikita\\IdeaProjects\\openCV\\src\\resources/" + filename, markedImage);
+        Imgcodecs.imwrite("src/resources/" + filename, markedImage);
 
 
         try {
 
-            tesseract.setDatapath("D:/Tess4J/tessdata");
+            tesseract.setDatapath("Tess4J/tessdata");
 
-            String text = tesseract.doOCR(new File("src/resources/Ouput.jpg"));
+            String text = tesseract.doOCR(new File("src/resources/plate_number.jpg"));
 
             System.out.print(text);
         }
         catch (TesseractException e) {
-            e.printStackTrace();
+            System.out.print("Ошибка");
         }
+
 
         System.exit(1);
     }
